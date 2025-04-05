0503680b6f295161141c34e182e3e5eb13dc2a52
Adityanpx <137485127+Adityanpx@users.noreply.github.com>
2025-04-05 20:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var isDate = function (input) {
-  //   write your code here
+  if (input instanceof Date && !isNaN(input)) {
+    return true; // जर हे वैध Date ऑब्जेक्ट असेल तर true परत करा
+  }
+
+  const parsedDate = new Date(input); // इनपुटला Date मध्ये कन्व्हर्ट करण्याचा प्रयत्न
+  return !isNaN(parsedDate); // जर तो वैध डेट असेल तर true, नाहीतर false
 };
 
 // Do not change the code below.
